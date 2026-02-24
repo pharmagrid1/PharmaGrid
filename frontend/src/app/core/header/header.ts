0c4074610e6fccb85f8e4994166c12c887b72e3f
anjezzaa <anjeza.murati@gmail.com>
2026-02-24 13:52:42 +0100

    Custom Pharmacy Theme Styling

--- frontend/src/app/core/header/header.ts
+++ frontend/src/app/core/header/header.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink } from "@angular/router";
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink],
+  imports: [RouterLink, MatButtonModule, MatToolbarModule],
   standalone: true,
   templateUrl: './header.html',
   styleUrl: './header.scss',
