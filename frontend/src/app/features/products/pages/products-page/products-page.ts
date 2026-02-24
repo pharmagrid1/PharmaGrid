401534a1297b6aa2541ce57d17cf60069e0554ec
Merita Aliu <ma30931@seeu.edu.mk>
2026-02-24 13:15:39 +0100

    feat(frontend): connect products page to backend API

--- frontend/src/app/features/products/pages/products-page/products-page.ts
+++ frontend/src/app/features/products/pages/products-page/products-page.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProductService, Product } from '../../product.service';
 
 @Component({
   selector: 'app-products-page',
-  imports: [],
+  imports: [CommonModule],
   standalone: true,
   templateUrl: './products-page.html',
   styleUrl: './products-page.scss',
 })
-export class ProductsPage {
+export class ProductsPage implements OnInit{
+  products : Product[]=[];
+
+  constructor(private productService: ProductService){}
+
+  ngOnInit(): void {
+    this.productService.getProducts().subscribe(data=>{
+      this.products=data;
+    });
+  }
 
 }
