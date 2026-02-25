d54a41c4f57295e26b2f5028f28bc8c7f2cadb45
Merita Aliu <ma30931@seeu.edu.mk>
2026-02-25 10:39:21 +0100

    first implement product filtering system (skin type & brand)

--- frontend/src/app/features/products/pages/products-page/products-page.ts
+++ frontend/src/app/features/products/pages/products-page/products-page.ts
@@ -2,10 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductService, Product } from '../../product.service';
 import { ProductCard } from '../../../../shared/product-card/product-card';
+import { MatAnchor } from "@angular/material/button";
 
 @Component({
   selector: 'app-products-page',
-  imports: [CommonModule, ProductCard],
+  imports: [CommonModule, ProductCard, MatAnchor],
   standalone: true,
   templateUrl: './products-page.html',
   styleUrl: './products-page.scss',
@@ -13,6 +14,9 @@ import { ProductCard } from '../../../../shared/product-card/product-card';
 export class ProductsPage implements OnInit{
   products : Product[]=[];
 
+  filteredProducts: Product[]=[];
+  selectedSkinType='';
+  selectedBrand='';
   
 
   constructor(private productService: ProductService){}
@@ -20,7 +24,39 @@ export class ProductsPage implements OnInit{
   ngOnInit(): void {
     this.productService.getProducts().subscribe(data=>{
       this.products=data;
+      this.filteredProducts=data;
     });
   }
+    
+    
+    filterBySkinType(event:any){
+      this.selectedSkinType=event.target.value;
+      this.applyFilters();
+    }
+    
 
+    filterByBrand(event: any){
+      this.selectedBrand=event.target.value;
+      this.applyFilters();
+    }
+
+    clearFilters(){
+      this.selectedSkinType='';
+      this.selectedBrand='';
+      this.filteredProducts=this.products;
+    }
+
+    applyFilters(){
+      this.filteredProducts=this.products.filter(product=>{
+        const matchesSkin=
+          !this.selectedSkinType||
+          product.skinType===this.selectedSkinType;
+
+        const matchesBrand=
+          !this.selectedBrand||
+          product.brand===this.selectedBrand;
+
+        return matchesSkin && matchesBrand;
+      });
+    }
 }
