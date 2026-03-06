59016c8c974629838dac3eac7856c5b76c998bc5
am30771 <am30771@seeu.edu.mk>
2026-03-06 12:52:35 +0100

    Admin Dashboard

---
+++ backend/routes/adminRoutes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const {protect, adminOnly} = require('../middleware/authMiddleware');
+const adminController = require('../controllers/adminController');
+
+//products
+
+router.get('./products', protect, adminOnly, adminController.getAllProducts);
+router.post('./products', protect, adminOnly, adminController.createProduct);
+router.put('./products/:id', protect, adminOnly, adminController.updateProduct);
+router.patch('./products/:id/deactivate', protect, adminOnly, adminController.deactivateProduct);
+
+//Orders
+router.get('/orders', protect, adminOnly, adminController.getAllProducts);
+router.patch('/orders/:id/status', protect, adminOnly, adminController.updateOrderStatus);
+
+module.exports = router;
