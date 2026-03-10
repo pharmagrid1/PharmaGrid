fcbe261516fa644bf04a0ae6218fa329625aaa10
am30771 <am30771@seeu.edu.mk>
2026-03-10 11:49:49 +0100

    fixing activate route in admin dashboard

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -9,6 +9,7 @@ router.get('/products', protect, adminOnly, adminController.getAllProducts);
 router.post('/products', protect, adminOnly, adminController.createProduct);
 router.put('/products/:id', protect, adminOnly, adminController.updateProduct);
 router.patch('/products/:id/deactivate', protect, adminOnly, adminController.deactivateProduct);
+router.patch('/products/:id/activate', protect, adminOnly, adminController.activateProduct);
 
 //Orders
 router.get('/orders', protect, adminOnly, adminController.getAllOrders);
